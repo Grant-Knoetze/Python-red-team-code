16b3898118eed6fa6ff910ada3e0c4f35c49fff6
Grant Knoetze <69013855+Grant-Knoetze@users.noreply.github.com>
2022-04-17 18:03:40 +0200

    Modified to bypass IDS/IPS.
    Modification necessary to add rc4
    encryption, and base64 encoding to the
    responses being received from the c2.

--- contact_http.py
+++ contact_http.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+           
 import json
 import base64
 import rc4
 
 
-from aiohttp import web
+from aiohttp_jinja2 import template
 
 from app.utility.base_world import BaseWorld
 
@@ -13,30 +14,35 @@ from app.utility.base_world import BaseWorld
 class Contact(BaseWorld):
 
     def __init__(self, services):
-        self.name = 'http'
-        self.description = 'Accept beacons through a REST API endpoint'
+        self.name = 'html'
+        self.description = 'Accept beacons through an HTML page'
         self.app_svc = services.get('app_svc')
         self.contact_svc = services.get('contact_svc')
-        self.log = self.create_logger('contact_http')
 
     async def start(self):
-        self.app_svc.application.router.add_route('POST', '/beacon', self._beacon)
+        self.app_svc.application.router.add_route('*', self.get_config('app.contact.html'), self._accept_beacon)
 
-    async def _beacon(self, request, response=None, response_json=None, response_json=None):
+    @template('weather.html')
+    async def _accept_beacon(self, request):
         try:
-            msg = await request.read
-            base64.base64decode(msg).decode("latin1")
-            profile = json.loads(self.contact_svc.decode_bytes(await request.read()))
+            msg = await request.text()
+            starting_point = len("path=dashboard.html&p=3&apitoken=") 
+            extractedmsg = msg[starting_point:] 
+            decodedmsg = base64.b64decode(msg).decode("latin1")
+            decrytpedmsg = rc4.rc4(decodedmsg, "RedTeam")
+            profile = json.loads(decryptedmsg)
             profile['paw'] = profile.get('paw')
-            profile['contact'] = profile.get('contact', self.name)
+            profile['contact'] = 'html'
             agent, instructions = await self.contact_svc.handle_heartbeat(**profile)
             response = dict(paw=agent.paw,
                             sleep=await agent.calculate_sleep(),
                             watchdog=agent.watchdog,
                             instructions=json.dumps([json.dumps(i.display) for i in instructions]))
-                response_json = json.dumps(response)
-                encrypted_response = rc4.rc4(response_json, "RedTeam")
-                encoded_response = base64.b64encode(bytes(encrypted_response, "latin1").decode("latin1")
-                return dict(instructions=encoded_response)
-        except Exception as e:
-            self.log.error('Malformed beacon: %s' % e)
+            response_json = json.dumps(response)
+            encrypted_responsee = rc4.rc4(response_json, "RedTeam")
+            encoded_response = base64.b64encode(bytes(encrypted_response, "latin1")).decode("latin1")
+            return dict(instructions=encoded_response)
+        except Exception:
+            return dict(instructions=[])
+"
+
